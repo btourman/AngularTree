1ea188f19c7fd5475d4d929457c3c5661aae577d
Tourman Benjamin <tourman.benjamin@gmail.com>
2015-02-01 00:48:07 +0100

    Use recursion to create complete tree

--- app/app.js
+++ app/app.js
@@ -2,13 +2,21 @@ var fs = require('fs');
 
 var json = JSON.parse(fs.readFileSync('directories.json', 'utf8'));
 
-json['directories'].forEach(function (directory) {
-	console.log(directory);
-});
+createTree(json['directories'], '');
 
- json['directories'].forEach(function (directory) {
- 	fs.mkdir(directory['name'], function (err) {
-		if(err) console.error(err);
-		else console.log(directory['name'] + " directory was created");
-	});
- });
+function createTree(directories, root) {
+    for (var i = 0; i < directories.length; ++i) {
+        var directory = directories[i];
+        var name = root + directory['name'];
+        fs.mkdirSync(name);
+
+        if (directory['children']) {
+            var children = directory['children'];
+            createTree(children, name + '/');
+            /*fs.mkdir(name + '/' + children['name'], function(err) {
+                if (err) console.error(err);
+                else console.log(name + '/' + children['name'] + " directory was created");
+            });*/
+        }
+    }
+}
